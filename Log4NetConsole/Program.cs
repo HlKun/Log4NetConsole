d23d47e08863ed86a2490b62b02c9c886ca7d837
HlKun <1104736831@qq.com>
2020-03-07 14:34:43 +0800

    Initial commit

---
+++ Log4NetConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            log.Error("error");
+            log.Fatal("fatal");
+            log.Info("info");
+            log.Debug("debug");
+            log.Warn("warn");
+            Console.WriteLine("日志记录完毕。");
+            Console.Read();
+        }
+    }
+}
